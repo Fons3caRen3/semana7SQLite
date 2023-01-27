e08d986f36f21decae4b3be9dd030d5e535fc8c0
Fausto Almeida <faustecn@hotmail.com>
2023-01-26 19:46:38 -0500

    Vista Consulta

--- semana7/semana7/ConsultaRegistro.xaml.cs
+++ semana7/semana7/ConsultaRegistro.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,38 @@ namespace semana7
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConsultaRegistro : ContentPage
     {
+        private SQLiteAsyncConnection con;//variable de conexión
+        private ObservableCollection<Estudiante> tEstudiante;
         public ConsultaRegistro()
         {
             InitializeComponent();
+            con = DependencyService.Get<DataBase>().GetConnection(); //inicializar variable conexion
+            Listar();
+        }
+
+        public async void Listar(){
+
+            var resultado=await con.Table<Estudiante>().ToListAsync();
+            tEstudiante= new ObservableCollection<Estudiante>(resultado);
+            ListaEstudiantes.ItemsSource=tEstudiante;
+        }
+
+        public void OnSelection(object sender, SelectedItemChangedEventArgs e){
+
+            var obj=(Estudiante)e.SelectedItem;
+            var item=obj.Id.ToString();
+            var Id=Convert.ToInt32(item);
+            var Nombre=obj.Nombre.ToString();
+            var Usuario=obj.Usuario.ToString();
+            var Contrasena= obj.Contrasena.ToString();
+
+            Navigation.PushAsync(new Elemento(Id,Nombre, Usuario, Contrasena));
+
+        }
+
+        private void BtnRegresar_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- semana7/semana7/Elemento.xaml.cs
+++ semana7/semana7/Elemento.xaml.cs
@@ -3,18 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
 
 namespace semana7
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Elemento : ContentPage
     {
-        public Elemento()
+
+        public int IdSeleccionado;
+        private SQLiteAsyncConnection con;
+
+        IEnumerable<Estudiante> rEliminar;
+        IEnumerable<Estudiante> rActualizar;
+
+        public Elemento(int Id, string Nombre, string Usuario, string Contrasena)
         {
             InitializeComponent();
+            txtNombre.Text = Nombre;
+            txtUsuario.Text = Usuario;
+            txtContrasena.Text= Contrasena;
+            IdSeleccionado= Id;
+        }
+
+        public static IEnumerable<Estudiante> Delete(SQLiteConnection db, int id) {
+            return db.Query<Estudiante>("DELETE FROM Estudiante WHERE Id=?", id);
+        }
+
+
+        public static IEnumerable<Estudiante> Update(SQLiteConnection db, int id, string nombre, string usuario, string contrasena)
+        {
+            return db.Query<Estudiante>("UPDATE Estudiante set Nombre=?, Usuario=?, Contrasena=? where Id=?", nombre,usuario,contrasena,id);
+        }
+
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db=new SQLiteConnection(ruta);
+                rActualizar = Update(db, IdSeleccionado, txtNombre.Text, txtUsuario.Text, txtContrasena.Text);
+                Navigation.PushAsync(new ConsultaRegistro());
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var ruta = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(ruta);
+                rEliminar = Delete(db, IdSeleccionado);
+                Navigation.PushAsync(new ConsultaRegistro());
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
         }
     }
 }
